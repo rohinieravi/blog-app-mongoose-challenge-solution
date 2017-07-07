c0a624d51741269ffcc7439d213beec6b3023ff0
rohinieravi <rohinieravi@yahoo.com>
2017-07-07 11:14:41 -0700

    added test

---
+++ test/test-blogs-integration.js
@@ -0,0 +1,243 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// this makes the should syntax available throughout
+// this module
+const should = chai.should();
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+// used to put randomish documents in db
+// so we have data to work with and assert about.
+// we use the Faker library to automatically
+// generate placeholder values for author, title, content
+// and then we insert that data into mongo
+function seedBlogData() {
+  console.info('seeding blog data');
+  const seedData = [];
+
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogData());
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+// used to generate data to put in db
+function generateAuthorData() {
+  const fName = faker.name.firstName();
+  const lName = faker.name.lastName();
+  return {
+    firstName: fName,
+    lastName: lName
+  };
+}
+
+// used to generate data to put in db
+function generateTitleData() {
+  return faker.lorem.words();
+}
+
+// used to generate data to put in db
+function generateContent() {
+  return faker.lorem.sentences();
+}
+
+
+// generate an object represnting a blog.
+// can be used to generate seed data for db
+// or request.body data
+function generateBlogData() {
+  return {
+    author: generateAuthorData(),
+    content: generateContent(),
+    title: generateTitleData()
+    //created: faker.date.past()
+  }
+}
+
+
+// this function deletes the entire database.
+// we'll call it in an `afterEach` block below
+// to ensure  ata from one test does not stick
+// around for next one
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('BlogPost API resource', function() {
+
+  // we need each of these hook functions to return a promise
+  // otherwise we'd need to call a `done` callback. `runServer`,
+  // `seedRestaurantData` and `tearDownDb` each return a promise,
+  // so we return the value returned by these function calls.
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBlogData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  })
+
+  // note the use of nested `describe` blocks.
+  // this allows us to make clearer, more discrete tests that focus
+  // on proving something small
+  describe('GET endpoint', function() {
+
+    it('should return all blog posts', function() {
+
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(_res) {
+          // so subsequent .then blocks can access resp obj.
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.length.of.at.least(1);
+          return BlogPost.count();
+        })
+        .then(function(count) {
+          res.body.should.have.length.of(count);
+        });
+    });
+
+
+    it('should return blogs with right fields', function() {
+
+      let resBlog;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.length.of.at.least(1);
+
+          res.body.forEach(function(post) {
+            post.should.be.a('object');
+            post.should.include.keys(
+              'id', 'title', 'content', 'author', 'created');
+          });
+          resBlog = res.body[0];
+          return BlogPost.findById(resBlog.id);
+        })
+        .then(function({id,title,content,author,created}) {
+          resBlog.id.should.equal(id);
+          resBlog.title.should.equal(title);
+          resBlog.content.should.equal(content);
+          resBlog.author.should.contain(author.firstName);
+          resBlog.author.should.contain(author.lastName);
+          //resBlog.created.should.equal(created);
+        });
+    });
+  });
+
+  describe('POST endpoint', function() {
+    // strategy: make a POST request with data,
+    // then prove that the restaurant we get back has
+    // right keys, and that `id` is there (which means
+    // the data was inserted into db)
+    it('should add a new post', function() {
+
+      const newPost = generateBlogData();
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newPost)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+              'id', 'title', 'content', 'author', 'created');
+          res.body.title.should.equal(newPost.title);
+          // cause Mongo should have created id on insertion
+          res.body.id.should.not.be.null;
+          res.body.content.should.equal(newPost.content);
+          res.body.title.should.equal(newPost.title);
+          res.body.author.should.contain(newPost.author.firstName);
+          res.body.author.should.contain(newPost.author.lastName);
+          
+          return BlogPost.findById(res.body.id);
+        })
+        .then(function({title, content, author, created}) {
+          title.should.equal(newPost.title);
+          content.should.equal(newPost.content);
+          author.firstName.should.equal(newPost.author.firstName)
+          author.lastName.should.equal(newPost.author.lastName);
+          //created.should.equal(newPost.created);
+        });
+    });
+  });
+
+  describe('PUT endpoint', function() {
+
+    it('should update fields you send over', function() {
+      const updateData = {
+        title: 'fofofofofofofof',
+        content: 'futuristic fusion'
+      };
+
+      return BlogPost
+        .findOne()
+        .exec()
+        .then(function(post) {
+          updateData.id = post.id;
+
+          // make request then inspect it to make sure it reflects
+          // data we sent
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          res.should.have.status(201);
+
+          return BlogPost.findById(updateData.id).exec();
+        })
+        .then(function({title, content}) {
+          title.should.equal(updateData.title);
+          content.should.equal(updateData.content);
+        });
+      });
+  });
+
+  describe('DELETE endpoint', function() {
+
+    it('delete a post by id', function() {
+
+      let post;
+
+      return BlogPost
+        .findOne()
+        .exec()
+        .then(function(_post) {
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return BlogPost.findById(post.id).exec();
+        })
+        .then(function(_post) {
+          should.not.exist(_post);
+        });
+    });
+  });
+});
